95f0df447398a0424c8a990b201d53b34ffa3996
shreyas18jan <shreyas18jan@gmail.com>
2021-11-01 09:49:30 +0530

    Adding TESTNG Changes

--- src/test/java/com/example/appium/AppiumDemoRunnerTest.java
+++ src/test/java/com/example/appium/AppiumDemoRunnerTest.java
@@ -2,12 +2,21 @@ package com.example.appium;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.junit.runner.RunWith;
+import org.testng.annotations.DataProvider;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features="src/test/java/com/example/appium/features",
         glue={"com.example.appium.stepDefinition"},
+        tags = "@Regression1",
         plugin = {"pretty", "html:target/cucumber-reports.html"})
-public class AppiumDemoRunnerTest {
+public class AppiumDemoRunnerTest extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
